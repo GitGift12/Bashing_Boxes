bc4bf93d0908b9a7c6895c223b5577e700444788
GitGift12 <KJani@smail.pembinatrails.ca>
2025-10-20 15:23:26 -0500

    Saved Work And Finished Echo Scripts

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 Frost=("Tie" "Menu" "Umbrela" "Dish" "Flame" "Padlock" "Shampoo" "Egg" "Shield" "Nachos")
+echo "1. Print List"
+echo "2. Print Item at Position"
+echo "3. Add Item"
+echo "4. Remove Last Item"
+echo "5. Remove Item at Position"
+echo "6. Exit"
+read -p "Choose an option: " choice
